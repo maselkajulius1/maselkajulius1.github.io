7b70255afb07540b61b6435656bf606713a3d4d4
Julius Maselka <maselkajulius@gmail.com>
2020-09-09 21:18:47 -0400

    changed words

---
+++ math.js
@@ -0,0 +1,39 @@
+function multiply() {
+    var numberOne = document.getElementById("numberOneInput").value 
+    var numberTwo = document.getElementById("numberTwoInput").value
+
+    var product = numberOne * numberTwo
+    
+    var resultElement = document.getElementById("result")
+    resultElement.innerText = 'result ' + product
+}
+
+function divide() {
+    var numberOne = document.getElementById("numberOneInput").value
+    var numberTwo = document.getElementById("numberTwoInput").value
+
+    var quotient = numberOne / numberTwo
+
+    var resultElement = document.getElementById("result")
+    resultElement.innerText = 'result ' + quotient
+}
+
+function add() {
+    var numberOne = Number(document.getElementById("numberOneInput").value)
+    var numberTwo = Number(document.getElementById("numberTwoInput").value)
+
+    var sum = numberOne + numberTwo
+
+    var resultElement = document.getElementById("result")
+    resultElement.innerText = 'result' + sum
+}
+
+function subtract() {
+    var numberOne = document.getElementById("numberOneInput").value
+    var numberTwo = document.getElementById("numberTwoInput").value
+
+    var difference = numberOne - numberTwo
+
+    var resultElement = document.getElementById("result")
+    resultElement.innerText = 'result' + difference
+}
